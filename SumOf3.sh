87b671d27f532f821d42aa9eb4df6e60f027385f
shraddhaS28 <shraddhavatscse@gmail.com>
2020-02-06 12:19:04 +0530

    Shraddha, sumof3.sh

--- SumOf3.sh
+++ SumOf3.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
+declare -A arr[]
 echo Enter a Length of array
 read N
-n=$num
-for((i=0; i<$n; i++))
+echo Enter values of the  array 
+for(( i=0; i<$N; i++ ))
 do
-   
+	read m
+	arr[i]=$m
+done
+for(( i=0; i < $N-2; i++ ))
+do
+	for(( j=i+1; i < $N-1; j++ ))
+	do
+		for(( k=j+1; k < $N; k++ ))
+		do
+			if(( $((arr[i]+arr[j]+arr[k]))==0 ))
+			then
+				echo $((arr[i])) $((arr[j])) $((arr[k]))
+			fi
+		done
+	done
 done
