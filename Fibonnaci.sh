8103f3edf64cc2c4c1f8dabc09f0f768513666ec
shraddhaS28 <shraddhavatscse@gmail.com>
2020-02-06 11:46:33 +0530

    waste

--- Fibonnaci.sh
+++ Fibonnaci.sh
@@ -3,11 +3,13 @@
 echo "Enter a Number"
 read N
 t1=0
-t2=0
-
-for((i=0; i>=$N; i++))
+t2=1
+echo $t1
+echo $t2
+for((i=2; i<=$N; i++))
 do
-temp=$t1+$t2
-
-
+	#temp=$((t1+t2))
+	echo $((t1+t2)) " "
+	t1=$t2
+	t2=$((t1+t2))
 done
