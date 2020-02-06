d60e31d280cfbe114fd8fd2eca535f352f10e087
SD7689 <dewanganshivam89@gmail.com>
2020-02-06 12:05:32 +0530

    [Shivam]Refaracted. Sum of 3 Program Till if Condition

--- SumOf3.sh
+++ SumOf3.sh
@@ -9,5 +9,14 @@ read a[$i]
 done
 for((i=0; i<n; i++))
 do
+echo "Enter value of Integer Array:"
 printf "$((a[$i])) "
 done
+for((i=0; i<n-2; i++))
+do
+	for((j=i+1; i<n-1; j++))
+	do
+		for((k=j+1; k<n; k++))
+		do
+			if(($((a[$i] + a[$j] + a[$k]))))
+			then
